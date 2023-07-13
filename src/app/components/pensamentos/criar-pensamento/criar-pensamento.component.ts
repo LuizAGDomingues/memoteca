71a2d60e02757e4015b4b32c41516e3eef6c1563
LuizAGDomingues <luizarthur1234@gmail.com>
2023-07-13 15:05:49 -0300

    feat: CRUD done

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -6,19 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
-  pensamento = {
-    id: '1',
+
+  pensamento: Pensamento = {
     conteudo: '',
     autoria: '',
     modelo: ''
   }
 
-  criarPensamento() {
-    alert("Novo pensamento criado")
-  }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) {}
 
-  constructor() {}
   ngOnInit(): void {
       
   }
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
 }
--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,22 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
-export class ListarPensamentoComponent {
+export class ListarPensamentoComponent implements OnInit{
 
-  listaPensamentos = [
-    {
-      conteudo: 'I Love Angular',
-      autoria: 'Dev',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'UOU',
-      modelo: 'modelo1'
-    }
-  ]
+  listaPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) {}
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
 }
